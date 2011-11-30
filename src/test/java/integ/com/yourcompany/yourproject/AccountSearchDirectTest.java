bd0978125305bbaba38115aba92336b85151c4ba
Florent Ramière <florent@ramiere.com>
2011-11-30 19:02:36 +0100

    sync functionnalities between fluentenium and selenium

--- src/test/java/integ/com/yourcompany/yourproject/AccountSearchDirectTest.java
+++ src/test/java/integ/com/yourcompany/yourproject/AccountSearchDirectTest.java
@@ -6,12 +6,16 @@ import static org.openqa.selenium.By.id;
 import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.name;
 
+import javax.annotation.Nullable;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.common.base.Function;
 
 public class AccountSearchDirectTest {
 
@@ -45,14 +49,31 @@ public class AccountSearchDirectTest {
                 return !d.findElement(id("searchResultsRegion")).getText().isEmpty();
             }
         });
+        new WebDriverWait(driver, 2).until(new PaginationAvailable("1 / 2"));
 
         assertThat(driver.findElement(id("searchResultsRegion")).getText()).isEqualTo("13 results");
         assertThat(driver.findElement(cssSelector("span.ui-paginator-current")).getText()).isEqualTo("1 / 2");
 
         driver.findElement(cssSelector("span.ui-icon-seek-next")).click();
+        new WebDriverWait(driver, 2).until(new PaginationAvailable("2 / 2"));
         assertThat(driver.findElement(cssSelector("span.ui-paginator-current")).getText()).isEqualTo("2 / 2");
 
         driver.findElement(cssSelector("span.ui-icon-seek-prev")).click();
+        new WebDriverWait(driver, 2).until(new PaginationAvailable("1 / 2"));
         assertThat(driver.findElement(cssSelector("span.ui-paginator-current")).getText()).isEqualTo("1 / 2");
     }
+
+    private final class PaginationAvailable implements Function<WebDriver, Boolean> {
+        private final String expected;
+
+        private PaginationAvailable(String expected) {
+            this.expected = expected;
+        }
+
+        @Override
+        public synchronized Boolean apply(@Nullable WebDriver driver) {
+            return expected.equals(driver.findElement(cssSelector("span.ui-paginator-current")).getText());
+        }
+    }
+
 }
